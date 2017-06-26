8209eb2c613b25e55769cf1335e713dd01cdd630
Márlon <marlonti17@gmail.com>
2017-06-26 17:09:26 -0300

    SP
    Foram substituidos inserçao de pessoa e produto por Stored Procedure

--- database.php
+++ database.php
@@ -33,8 +33,7 @@
 					   sha1("hello.4n".
 					   sha1("criptografado_comsucesso".$senha))))));
 
-			$sql = "INSERT INTO PESSOA (nome, data_nascimento, sexo, email, senha)
-					VALUES ('$nome','$data_nascimento', '$sexo', '$email', '$senha_c')";
+			$sql = "CALL sp_inserir_pessoa('$nome','$data_nascimento', '$sexo', '$email', '$senha_c');";
 			
 			if($this->conexao->query($sql) === TRUE){
 				$ultimo_id = $this->conexao->insert_id;
@@ -45,13 +44,7 @@
 			}
 		}
 
-		function inserirProduto($nome, $valor, $categoria, $marca, $id_pessoa){
-
-			//faz um select pra ver se essa $marca ja existe, e se existe
-			//pega o id dela no banco. Caso n'ao exista, insere ela no banco
-			//e pega o id da marca inserida		
-
-
+		function inserirProduto($nome, $valor, $categoria, $marca, $id_pessoa){	
 
 			//Busca idMarca
 			$query_marca = "SELECT idMarca FROM MARCA WHERE nome_marca = '$marca'";
@@ -74,8 +67,7 @@
 				$id_marca = $this->conexao->insert_id;
 			}
 
-			$query_inserir_produto = "INSERT INTO PRODUTO(nome, valor, categoria, dataPublicacao, idPessoa , idMarca) 
-				VALUES ('$nome', '$valor', '$categoria', CURDATE(), '$id_pessoa', '$id_marca')";
+			$query_inserir_produto = "CALL sp_inserir_produto('$nome', '$valor', '$categoria', CURDATE(), '$id_pessoa', '$id_marca');";
 			$this->conexao->query($query_inserir_produto);
 		}
 
@@ -118,45 +110,12 @@
 			if($resultado->num_rows > 0){
 				// mostra os dados de cada linha retornada
 				while($linha = $resultado->fetch_assoc()) {
-					/*echo "<br>Nome: " . $linha["email"]. " " .$linha["senha"];
-					$senha_c = sha1(
-						sha1("criptografia_nivel.1".
-						sha1("nivel_2".
-						sha1("n3.criptografia".
-						sha1("hello.4n".
-						sha1("criptografado_comsucesso".$senha))))));*/
-					/*
-					if($email == $linha["email"] && $senha == $senha_c){
-						header("location:index.html");
-					}*/
 					return $linha['idPessoa'];
 				}
 			}
 			else{
 				return -1;
 				//echo "Nenhum resultado encontrado";
-			}
-		}
-		
-		function alterarEmail($id, $email){
-			// Alteração de um dado da tabela
-			$sql = "UPDATE Pessoa SET email='$email' WHERE id = $id";
-			if($this->conexao->query($sql) === TRUE){
-				echo "<BR>Registro alterado com sucesso!";
-			}
-			else{
-				echo "<br>Erro na alteração do registro: " . $this->conexao->error;
-			}
-		}
-
-		function alterarSenha($id, $senha){
-			// Alteração de um dado da tabela
-			$sql = "UPDATE Pessoa SET senha='$senha' WHERE id = $id";
-			if($this->conexao->query($sql) === TRUE){
-				echo "<BR>Registro alterado com sucesso!";
-			}
-			else{
-				echo "<br>Erro na alteração do registro: " . $this->conexao->error;
 			}
 		}
 		
